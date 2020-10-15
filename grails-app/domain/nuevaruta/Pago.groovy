a6dad2d5d006e9f90dd83823d8595624586381f8
jcardenasvar <44623090+jcardenasvar@users.noreply.github.com>
2020-10-15 10:43:05 -0300

    Update Pago.groovy

--- grails-app/domain/nuevaruta/Pago.groovy
+++ grails-app/domain/nuevaruta/Pago.groovy
@@ -1,7 +1,7 @@
 package nuevaruta
 
 class Pago {
-    String banco, tipoCuenta, monto, observacion, estado, tipoTarjeta, nTarjeta, fechaVencimiento, formadepago, error
+    String banco, tipoCuenta, monto, observacion, estado, tipoTarjeta, numeroTarjeta, fechaVencimiento, formadepago, error, fechaPago, recepcion
     static belongsTo = [reserva:Reserva, cliente:Cliente]
     static constraints = {
         estado nullable: true, blank: true
